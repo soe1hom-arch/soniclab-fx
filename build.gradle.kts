fb12cd73c76f49a435e128ab41f4d97d73c51b45
soe1hom-arch <soe1hom-arch@users.noreply.github.com>
2026-08-26 14:11:26 +0700

    fix: Use matching Kotlin Compose plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.5.11" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
 }
